e2933569c07210c7bc8a18f1880890df3e41ff42
Good Evening <114830237+GoodEveningFellOff@users.noreply.github.com>
2025-10-12 13:09:20 -0400

    Had this lying around, figured I should push it

--- Rainbow Colors/rainbow.lua
+++ Rainbow Colors/rainbow.lua
@@ -1,23 +1,91 @@
-local Config = {
-	Speed = 1; -- go crazy -- Default 1
-	Saturation = 0.75; -- between 0 and 1 -- Default 0.75
-	Lightness = 1; -- between 0 and 1 -- Default 1
+local function Rainbow(flSpeed, flSaturation, flLightness, iAlpha)return{(type(tonumber(flSpeed)) ~= "number") and 1 or tonumber(flSpeed);(type(tonumber(flSaturation)) ~= "number") and 1 or math.min(math.max(math.abs(tonumber(flSaturation)), 0), 1);(type(tonumber(flLightness)) ~= "number") and 1 or math.min(math.max(math.abs(tonumber(flLightness)), 0), 1);(type(tonumber(iAlpha)) ~= "number") and 255 or math.floor(math.min(math.max(math.abs(tonumber(iAlpha)), 0), 255));};end
+local function Static(iR, iG, iB, iA)return(((type(tonumber(iR)) ~= "number") and 255 or math.floor(math.min(math.max(math.abs(tonumber(iR)), 0), 255))) << 24) | (((type(tonumber(iG)) ~= "number") and 255 or math.floor(math.min(math.max(math.abs(tonumber(iG)), 0), 255))) << 16) | (((type(tonumber(iB)) ~= "number") and 255 or math.floor(math.min(math.max(math.abs(tonumber(iB)), 0), 255))) << 8) | ((type(tonumber(iA)) ~= "number") and 255 or math.floor(math.min(math.max(math.abs(tonumber(iA)), 0), 255)));end
+local function Disabled()return false;end
 
+local config = {
+    -- If true: use enemy_or_red and friend_or_blue for red and blue teams.
+    -- If false: use enemy_or_red and friend_or_blue for enemy and friend.
+    team_based = false;
 
-	-- Set to true to rainbowize ;)
-	Backtrack = true; 
-	BlueTeam = true;
-	BlueTeamInvis = false;
-	RedTeam = true;
-	RedTeamInvis = false;
-	Target = true;
-	Gui = true;
-	AAIndicator = false;
-	-- Night mode was removed due to it lagging the shit out of your game
-	-- Set to true to rainbowize
+    --[[
+    To have an item with the rainbow effect use 
+        Rainbow(speed [-inf, inf], saturation [0, 1], lightness [0, 1], alpha [0, 255]);
+
+    To have an item with a static color use
+        Static(red [0, 255], green [0, 255], blue [0, 255], alpha [0, 255]);
+
+    To disable changing of a color use
+        Disabled();
+    ]]
+    enemy_or_red = Rainbow(1, 0.75, 1, 255);
+    enemy_or_red_invis = Rainbow(1, 0.5, 0.5, 127);
+    friend_or_blue = Static(0, 127, 255, 255);
+    friend_or_blue_invis = Static(0, 127, 255, 127);
+    backtrack = Rainbow(1, 0.75, 1, 55);
+    target = Rainbow(10, 1, 1, 255);
+    gui = Rainbow(1, 0.8, 1, 255);
+    hands = Disabled();
+    anti_aim_indicator = Disabled();
+
+    -- Using a rainbow color on night mode may lead to lag.
+    night_mode = Disabled();
 };
 
+local g_bOnBlueTeam = false;
 
-local a,b,c,d,e,f,g=(function()local a,b,c,d,e={},math.floor,math.sin,255*Config.Lightness*Config.Saturation/2,255*(Config.Lightness-(Config.Lightness*Config.Saturation/2));for i=0,6.27882198405,0.00436332312999 do table.insert(a,b(c(i)*d+e) << 24 | b(c(i+2.1)*d+e) << 16 | b(c(i+4.2)*d+e) << 8 | 255)end return a;end)(),math.floor,globals.CurTime,gui.SetValue,gui.GetValue,Config.Speed*240,{};
-for k,v in pairs({["backtrack ticks color"]=Config.Backtrack;["blue team color"]=Config.BlueTeam;["blue team (invisible)"]=Config.BlueTeamInvis;["red team color"]=Config.RedTeam;["red team (invisible)"]=Config.RedTeamInvis;["aimbot target color"]=Config.Target;["gui color"]=Config.Gui;["anti aim indicator color"]=Config.AAIndicator;})do if v then table.insert(g,k)end end
-callbacks.Register("Draw",function()local h=a[(b(c()*f)%1439)+1];if h==e(g[1])then return end for _,i in pairs(g)do d(i,h)end end)
+local g_aSetData = {
+    [true] = {
+        ["backtrack indicator color"] = config.backtrack;
+        ["blue team color"] = config.friend_or_blue;
+        ["blue team (invisible)"] = config.friend_or_blue_invis;
+        ["red team color"] = config.enemy_or_red;
+        ["red team (invisible)"] = config.enemy_or_red_invis;
+        ["aimbot target color"] = config.target;
+        ["gui color"] = config.gui;
+        ["hands color"] = config.hands;
+        ["anti aim indicator color"] = config.anti_aim_indicator;
+        ["night mode color"] = config.night_mode;
+    };
+
+    [false] = {
+        ["backtrack indicator color"] = config.backtrack;
+        ["red team color"] = config.friend_or_blue;
+        ["red team (invisible)"] = config.friend_or_blue_invis;
+        ["blue team color"] = config.enemy_or_red;
+        ["blue team (invisible)"] = config.enemy_or_red_invis;
+        ["aimbot target color"] = config.target;
+        ["gui color"] = config.gui;
+        ["hands color"] = config.hands;
+        ["anti aim indicator color"] = config.anti_aim_indicator;
+        ["night mode color"] = config.night_mode;
+    };
+};
+
+callbacks.Register("Draw", function()
+    local pLocalPlayer = entities.GetLocalPlayer();
+    if(pLocalPlayer)then
+        g_bOnBlueTeam = pLocalPlayer:GetTeamNumber() ~= 2;
+    end
+
+    local flCurTime = globals.CurTime();
+    
+    for sElement, tData in pairs(g_aSetData[config.team_based or g_bOnBlueTeam])do
+        local iValue = gui.GetValue(sElement);
+        
+        if(type(tData) == "number")then
+            if(iValue ~= tData)then
+                gui.SetValue(sElement, tData);
+            end
+
+        elseif(type(tData) == "table")then
+            local flTime, flV1, flV2 = flCurTime * tData[1], 255 * tData[3] * tData[2] / 2, 255 * (tData[3] - (tData[3] * tData[2] / 2));
+            local iNewColor = (math.floor(math.sin(flTime) * flV1 + flV2) << 24) | (math.floor(math.sin(flTime + 2.09439510239) * flV1 + flV2) << 16) | (math.floor(math.sin(flTime + 4.18879020479) * flV1 + flV2) << 8) | tData[4]
+            
+            if(iValue ~= iNewColor)then
+                gui.SetValue(sElement, iNewColor);
+            end
+        end
+
+    
+    end
+end);
